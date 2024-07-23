c69412ff513b3c7b023b49275681f0c96d200248
ChandanTech2023 <chandankumar13102005@gmail.com>
2024-07-23 21:33:31 +0530

    add move_cursor feature

--- Personal_Portfolio/script.js
+++ Personal_Portfolio/script.js
@@ -1,95 +1,104 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    //sticky header
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > 1) {
-          $(".header-area").addClass("sticky");
-        } else {
-          $(".header-area").removeClass("sticky");
-        }
+  //sticky header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $(".header-area").addClass("sticky");
+    } else {
+      $(".header-area").removeClass("sticky");
+    }
 
-        // Update the active section in the header
-        updateActiveSection();
-      });
+    // Update the active section in the header
+    updateActiveSection();
+  });
 
-      $(".header ul li a").click(function(e) {
-        e.preventDefault(); 
+  $(".header ul li a").click(function (e) {
+    e.preventDefault();
 
-        var target = $(this).attr("href");
+    var target = $(this).attr("href");
 
-        if ($(target).hasClass("active-section")) {
-          return; 
-        }
+    if ($(target).hasClass("active-section")) {
+      return;
+    }
 
-        if (target === "#home") {
-          $("html, body").animate(
-            {
-              scrollTop: 0 
-            },
-            500
-          );
-        } else {
-          var offset = $(target).offset().top - 40; 
+    if (target === "#home") {
+      $("html, body").animate(
+        {
+          scrollTop: 0
+        },
+        500
+      );
+    } else {
+      var offset = $(target).offset().top - 40;
 
-          $("html, body").animate(
-            {
-              scrollTop: offset
-            },
-            500
-          );
-        }
+      $("html, body").animate(
+        {
+          scrollTop: offset
+        },
+        500
+      );
+    }
 
-        $(".header ul li a").removeClass("active");
-        $(this).addClass("active");
-      });
+    $(".header ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
 
 
-      // Initial content revealing js
-      ScrollReveal({
-        distance: "100px",
-        duration: 2000,
-        delay: 300
-      });
+  // Initial content revealing js
+  ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 300
+  });
 
-      ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
-        origin: "left"
-      });
-      ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
-        origin: "right"
-      });
-      ScrollReveal().reveal(".project-title, .contact-title", {
-        origin: "top"
-      });
-      ScrollReveal().reveal(".projects, .contact", {
-        origin: "bottom"
-      });
+  ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
+    origin: "left"
+  });
+  ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
+    origin: "right"
+  });
+  ScrollReveal().reveal(".project-title, .contact-title", {
+    origin: "top"
+  });
+  ScrollReveal().reveal(".projects, .contact", {
+    origin: "bottom"
+  });
 
-    //contact form to excel sheet
+  //contact form to excel sheet
 
 });
 
-    function updateActiveSection() {
-      var scrollPosition = $(window).scrollTop();
+function updateActiveSection() {
+  var scrollPosition = $(window).scrollTop();
 
-      // Checking if scroll position is at the top of the page
-      if (scrollPosition === 0) {
-        $(".header ul li a").removeClass("active");
-        $(".header ul li a[href='#home']").addClass("active");
-        return;
-      }
+  // Checking if scroll position is at the top of the page
+  if (scrollPosition === 0) {
+    $(".header ul li a").removeClass("active");
+    $(".header ul li a[href='#home']").addClass("active");
+    return;
+  }
 
-     // Iterate through each section and update the active class in the header
-      $("section").each(function() {
-        var target = $(this).attr("id");
-        var offset = $(this).offset().top;
-        var height = $(this).outerHeight();
+  // Iterate through each section and update the active class in the header
+  $("section").each(function () {
+    var target = $(this).attr("id");
+    var offset = $(this).offset().top;
+    var height = $(this).outerHeight();
 
-        if (
-          scrollPosition >= offset - 40 &&
-          scrollPosition < offset + height - 40
-        ) {
-          $(".header ul li a").removeClass("active");
-          $(".header ul li a[href='#" + target + "']").addClass("active");
-        }
-      });
+    if (
+      scrollPosition >= offset - 40 &&
+      scrollPosition < offset + height - 40
+    ) {
+      $(".header ul li a").removeClass("active");
+      $(".header ul li a[href='#" + target + "']").addClass("active");
     }
+  });
+}
+// Moveable Cursor button 
+let main = document.querySelector("#home")
+let cur = document.querySelector(".move-cursor")
+main.addEventListener("mousemove", function (dets) {
+  // console.log(dets.y)
+  cur.style.left = dets.x + "px"
+  cur.style.top = dets.y + "px"
+
+})
